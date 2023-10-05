e69ea7788a3e3cfd96181111b5b6a85f56e8a42e
João Almeida <joaofilipe90@gmail.com>
2023-10-05 11:21:48 +0100

    more tests

--- tests/tests.py
+++ tests/tests.py
@@ -4,13 +4,14 @@ import json
 
 def test_focusing_pregnancy():
     # Opening JSON file
-    f = open('../fsh-generated/resources/Bundle-processedbundledovato-en.json')
+    #f = open('../fsh-generated/resources/Bundle-processedbundledovato-en.json')
     
     # returns JSON object as 
     # a dictionary
-    data = json.load(f)
+    #data = json.load(f)
     x=requests.post("http://fosps.gravitatehealth.eu/focusing/focus/bundlepackageleaflet-2d49ae46735143c1323423b7aea24165?preprocessors=preprocessing-service-manual&lenses=lens-selector-mvp2_pregnancy&patientIdentifier=alicia-1")
-    return True
+    assert x.status_code == 200
+    
     
 #def test_calculate_area_square_negative():  
 #    with pytest.raises(TypeError):  
